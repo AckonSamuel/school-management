e2e475cf3a7f23056da30d0ba6cea7c988fdf17f
Samuel Ackon <ackons433@gmail.com>
2023-12-16 17:25:51 +0000

    Create model for subject

---
+++ app/Models/Subject.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Subject extends Model
+{
+    use HasFactory;
+
+    public $fillable = [
+        'name',
+        'description',
+        'semester',
+        'subject_code',
+        'teacher_id',
+        'classroom_id',
+    ];
+
+    public function classroom()
+    {
+        return $this->belongsTo(Classroom::class);
+    }
+
+    public function teacher()
+    {
+        return $this->belongsTo(Teacher::class);
+    }
+}
