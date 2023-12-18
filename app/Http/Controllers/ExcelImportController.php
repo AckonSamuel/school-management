b0a453e27a8ac9d1e76b8f08a52ed51e3da194c5
Samuel Ackon <ackons433@gmail.com>
2023-12-18 11:37:43 +0000

    Define import logic

--- app/Http/Controllers/ExcelImportController.php
+++ app/Http/Controllers/ExcelImportController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Imports\YourImportClass; // Assuming YourImportClass is the class that handles your import logic
 
 class ExcelImportController extends Controller
 {
-    //
+    public function import(Request $request)
+    {
+        // Validate the uploaded file
+        $request->validate([
+            'file' => 'required|mimes:xlsx,xls',
+        ]);
+ 
+        // Get the uploaded file
+        $file = $request->file('file');
+ 
+        try {
+            // Process the Excel file
+            Excel::import(new YourImportClass, $file);
+            
+            // Return a success response
+            return response()->json(['message' => 'Excel file imported successfully'], 200);
+        } catch (\Exception $e) {
+            // Return an error response if something goes wrong
+            return response()->json(['error' => 'Failed to import Excel file'], 500);
+        }
+    }
 }
