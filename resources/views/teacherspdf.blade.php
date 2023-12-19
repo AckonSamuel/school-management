478d6d8adfeede5184a0f484698383ca20040493
Samuel Ackon <ackons433@gmail.com>
2023-12-19 08:50:57 +0000

    Add seed command to procfile

--- resources/views/teacherspdf.blade.php
+++ resources/views/teacherspdf.blade.php
@@ -13,28 +13,19 @@
         <table class="table table-bordered mb-5">
             <thead>
                 <tr class="table-danger">
-                    <th scope="col">#</th>
                     <th scope="col">First Name</th>
                     <th scope="col">Last Name</th>
                     <th scope="col">email</th>
                     <th scope="col">phone_number</th>
-                    <th scope="col">birthday</th>
-                    <th scope="col">birthday</th>
-                    <th scope="col">gender</th>
-                    <th scope="col">address</th>
                 </tr>
             </thead>
             <tbody>
     @foreach($teachers ?? [] as $data)
     <tr>
-        <th scope="row">{{ $data['id'] }}</th>
         <td>{{ $data['first_name'] }}</td>
         <td>{{ $data['last_name'] }}</td>
         <td>{{ $data['email'] }}</td>
         <td>{{ $data['phone_number'] }}</td>
-        <td>{{ $data['birthday'] }}</td>
-        <td>{{ $data['gender'] }}</td>
-        <td>{{ $data['address'] }}</td>
     </tr>
     @endforeach
 </tbody>
