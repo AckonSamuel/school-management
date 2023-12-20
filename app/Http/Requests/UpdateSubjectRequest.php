2fefaee2e3d85844329162f5f5dea7335ea9d6d5
Samuel Ackon <ackons433@gmail.com>
2023-12-20 03:33:49 +0000

    Add Update student validation rules

--- app/Http/Requests/UpdateSubjectRequest.php
+++ app/Http/Requests/UpdateSubjectRequest.php
@@ -4,25 +4,31 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateSubjectRequest extends FormRequest
+class UpdateStudentRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
      */
     public function authorize(): bool
     {
-        return false;
+        return true; // Adjust authorization logic if needed
     }
 
     /**
      * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
-            //
+            'first_name' => 'required|max:30',
+            'surname' => 'required|max:30',
+            'birth_date' => 'required',
+            'classroom_id' => 'required|exists:classrooms,id',
+            'parent_phone_number' => 'required',
+            'second_phone_number' => 'nullable',
+            'enrollment_date' => 'required',
+            'address' => 'required',
+            'gender' => 'required',
         ];
     }
 }
