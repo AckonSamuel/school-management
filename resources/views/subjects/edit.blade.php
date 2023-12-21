10087a96b8334cec1a96f0beba2d942660e70aa5
Ackon Samuel <ackons433@gmail.com>
2023-12-21 18:51:05 +0000

    Update edit.blade.php

--- resources/views/subjects/edit.blade.php
+++ resources/views/subjects/edit.blade.php
@@ -1,1 +1,55 @@
-<?php
+@extends('home.app')
+
+@section('content')
+    <div class="py-6">
+        <h1 class="text-2xl font-bold mb-4">Edit Classroom</h1>
+
+        @if ($errors->any())
+            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative mb-4" role="alert">
+                <strong class="font-bold">Oops! There were some errors:</strong>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <div class="mt-2 mb-4 flex justify-between">
+            <a href="{{ route('classrooms.index', $classroom->id) }}" class="flex items-center text-black ml-4 hover:underline">
+                <svg class="w-4 h-4 inline-block mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18"></path>
+                </svg>
+                Return
+            </a>
+        </div>
+
+        <form action="{{ route('classrooms.update', $classroom->id) }}" method="POST" enctype="multipart/form-data"
+              class="max-w-md mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            @csrf
+            @method('PUT')
+
+            <div class="mb-4">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="class_name">Classroom Name:</label>
+                <input type="text" id="class_name" name="class_name" value="{{ $classroom->class_name }}"
+                       class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                       maxlength="30">
+            </div>
+
+            <div class="mb-4">
+            <label for="description" class="block text-gray-700 text-sm font-bold mb-2">Classroom Description:</label>
+            <textarea id="description" name="description"
+                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">{{ old('description') }}</textarea>
+            @error('description')
+            <p class="text-red-500 text-xs italic">{{ $message }}</p>
+            @enderror
+        </div>
+
+            <div class="flex items-center justify-between">
+                <button type="submit"
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Update
+                </button>
+            </div>
+        </form>
+    </div>
+@endsection
