fbfd504bffbc5323b37e2b7d0c8f590b12c57c45
Samuel Ackon <ackons433@gmail.com>
2023-12-21 17:34:15 +0000

    hot fix

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
+  base: 'https://dotnut-9337e56f7347.herokuapp.com/', // Set your base URL here
 });
