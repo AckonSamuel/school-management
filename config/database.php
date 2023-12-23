e22e88b06c5666934df75204671ccfb5008640f3
Samuel Ackon <ackons433@gmail.com>
2023-12-23 09:10:24 +0000

    Add database

--- config/database.php
+++ config/database.php
@@ -45,11 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('STACKHERO_MYSQL_HOST'),
-            'port' => 3306,
-            'username' => env('STACKHERO_MYSQL_USER'),
-            'password' => env('STACKHERO_MYSQL_PASSWORD'),
-            'database' => env('STACKHERO_MYSQL_USER'),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', 'db4free.net'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'schoolm'),
+            'username' => env('DB_USERNAME', 'new_user'),
+            'password' => env('DB_PASSWORD', 'Godisgreat@123.'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
